ff88c25e7cae839dbb362d8f6f45923f458195c5
Abduljebar <Abduljebar49@gmail.com>
2023-12-06 16:25:47 +0300

    add api to every endpoint

--- react-frontend/src/services/api-client.ts
+++ react-frontend/src/services/api-client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import useAuth from "../hooks/useAuth";
 
 export default axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "http://localhost:3000/api/",
   headers: {
     Authorization: `Bearer ${useAuth()?.token}`,
   },
